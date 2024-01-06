b086f2361fd86bbb852d85be16628f06b7975599
Takeshi Yokemura <yokemura@ymck.net>
2024-01-06 17:37:46 +0900

    Removed unused imports

--- lib/ui/page/home_page.dart
+++ lib/ui/page/home_page.dart
@@ -1,8 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:karamemo/model/state/home_state.dart';
 import 'package:karamemo/ui/component/atom/create_memo_button_body.dart';
@@ -14,6 +11,8 @@ import '../../model/view_data/memo.dart';
 import '../../model/view_data/memo_type.dart';
 
 class HomePage extends HookConsumerWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(homeControllerProvider);
